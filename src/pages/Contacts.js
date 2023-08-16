7eaf4edae0df6508a35470bb05cab867f3242c3e
cristiano <crizatip@gmail.com>
2023-08-15 21:12:47 -0300

    ajuste nos textos

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -6,7 +6,7 @@ class Contacts extends React.Component{
     render(){
         return(
             <div className='Contact'>
-                <h2>Onde me contactar</h2>
+                <h2>Meus contatos</h2>
             
                 <div className='Contact-icons'>
                     <a href="https://github.com/crizatip" target="_blank" rel="noreferrer">
@@ -16,9 +16,9 @@ class Contacts extends React.Component{
                         <img src={linkedin} alt="logo linkedin"></img>
                     </a>
                 </div>
-                <p>Caso tenha alguma pergunta, duvida ou oportindade pode me enviar um e-mail agora mesmo!</p>
+                <p>Caso queira me contratar ou possua alguma dúvida, estou disponível.</p>
                
-                <a className='Contact-mail' href = "mailto:crizatip@gmail.com">Enviar Email</a>
+                <a className='Contact-mail' href = "mailto:crizatip@gmail.com">Enviar e-mail</a>
 
                 <p>Design and built <br></br> by Cristiano Pita</p>
 
